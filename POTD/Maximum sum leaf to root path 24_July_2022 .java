64592f41013d9c2b19d715de9d16223bc8a13eca
Prashant Kumar <60030018+prashantt17@users.noreply.github.com>
2022-07-24 00:33:34 +0530

    Create Maximum sum leaf to root path 24_July_2022 .java

---
+++ POTD/Maximum sum leaf to root path 24_July_2022 .java
@@ -0,0 +1,60 @@
+/*
+Given a Binary Tree, find the maximum sum path from a leaf to root.
+
+
+Example 1:
+
+Input:
+        1
+       / \
+      2   3 
+Output:
+4
+Explanation: 
+Following the path 3 -> 1, results in a
+sum of 4, which is the maximum path sum
+from leaf to root for the given tree.
+Example 2:
+
+Input:
+       10
+      /  \
+    -2    7
+    / \   
+   8  -4    
+Output:
+17
+Explanation : 
+Following the path 7 -> 10, results in a
+sum of 17, which is the maximum path sum
+from leaf to root for the given tree.
+
+Your task :
+You don't need to read input or print anything. Your task is to complete the function maxPathSum() which takes the root node of the tree as input and returns an integer denoting the maximum possible leaf to root path sum.
+
+Expected Time Complexity: O(n) , where n = number of nodes
+Expected Auxiliary Space: O(1)
+
+Constraints : 
+1 <= Number of nodes <= 10^5
+*/
+
+
+class Solution
+{
+    public static int maxPathSum(Node root)
+    {
+        //code here
+        if (root == null) {
+            return 0;
+        }
+        
+        int rootVal = root.data;
+        int maxSum = rootVal+ Math.max(maxPathSum(root.left), maxPathSum(root.right));
+        
+        return maxSum;
+    }
+}
+
+
+
